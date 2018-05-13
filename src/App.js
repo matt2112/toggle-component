53ca2c733ffc7d64ed679d0ee6dd9e1248a25f57
Matt Lewis <matt_lewis@live.co.uk>
2018-05-13 17:29:33 +0100

    implement essential functionality

--- src/App.js
+++ src/App.js
@@ -1,27 +1,90 @@
 // @flow
 import React, { Component } from 'react';
+import cloneDeep from 'lodash/cloneDeep';
+import uniqueId from 'lodash/uniqueId';
 import './App.css';
 
 import Slider from './Slider';
 
-class App extends Component {
+type Props = {};
+
+type State = {
+  allCorrect: boolean,
+  question: string,
+  options: Array<{
+    correct: string,
+    incorrect: string,
+    correctOnLeft: boolean,
+    toggleOnLeft: boolean
+  }>
+};
+
+class App extends Component<Props, State> {
   state = {
-    correct: false,
-    question: 'What is the answer?'
+    allCorrect: false,
+    question: 'An animal cell contains:',
+    options: [
+      {
+        correct: 'Ribosomes',
+        incorrect: 'Cell wall',
+        correctOnLeft: false,
+        toggleOnLeft: false
+      },
+      {
+        correct: 'Cytoplasm',
+        incorrect: 'Chloroplast',
+        correctOnLeft: true,
+        toggleOnLeft: false
+      },
+      {
+        correct: 'Partially permeable membrane',
+        incorrect: 'Impermeable membrane',
+        correctOnLeft: true,
+        toggleOnLeft: false
+      },
+      {
+        correct: 'Cellulose',
+        incorrect: 'Mitochondria',
+        correctOnLeft: false,
+        toggleOnLeft: true
+      }
+    ]
+  };
+
+  checkIfAllCorrect = () => {
+    const allCorrect = this.state.options.every(
+      option => option.correctOnLeft === option.toggleOnLeft
+    );
+    this.setState({
+      allCorrect
+    });
+  };
+
+  clickSlider = (idx: number) => {
+    this.setState(
+      (prevState) => {
+        const options = cloneDeep(prevState.options);
+        options[idx].toggleOnLeft = !options[idx].toggleOnLeft;
+        return {
+          options
+        };
+      },
+      () => this.checkIfAllCorrect()
+    );
   };
 
   render() {
-    const { correct, question } = this.state;
+    const { allCorrect, question, options } = this.state;
     return (
       <div id="App">
         <div id="Wrapper">
           <h1 id="Question">{question}</h1>
           <div id="Sliders">
-            <Slider />
-            <Slider />
-            <Slider />
+            {options.map((option, idx) => (
+              <Slider key={uniqueId('slider')} index={idx} onClick={this.clickSlider} {...option} />
+            ))}
           </div>
-          <h2 id="Answer">The answer is {correct ? 'correct!' : 'incorrect.'}</h2>
+          <h2 id="Answer">The answer is {allCorrect ? 'correct!' : 'incorrect.'}</h2>
         </div>
       </div>
     );
--- src/Slider.js
+++ src/Slider.js
@@ -3,11 +3,29 @@ import React from 'react';
 
 import './Slider.css';
 
-const Slider = () => (
-  <div className="slider-outer">
-    <div className="slider-inner" />
-    <h3 className="text-left">Text left</h3>
-    <h3 className="text-right">Text right</h3>
+type Props = {
+  correct: string,
+  incorrect: string,
+  correctOnLeft: boolean,
+  toggleOnLeft: boolean,
+  index: number,
+  onClick: (idx: number) => void
+};
+
+const Slider = (props: Props) => (
+  <div
+    className="slider-outer"
+    onClick={() => props.onClick(props.index)}
+    onKeyPress={() => props.onClick(props.index)}
+    role="slider"
+    tabIndex="0"
+    aria-valuemin={0}
+    aria-valuemax={1}
+    aria-valuenow={props.toggleOnLeft ? 'left' : 'right'}
+  >
+    <div className={`slider-inner ${props.toggleOnLeft ? 'left-toggle' : 'right-toggle'}`} />
+    <h3 className="text-left">{props.correctOnLeft ? props.correct : props.incorrect}</h3>
+    <h3 className="text-right">{props.correctOnLeft ? props.incorrect : props.correct}</h3>
   </div>
 );
 
