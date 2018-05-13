f060e8a05ed17d6ac91a889a81ad37b15c7b4582
Matt Lewis <matt_lewis@live.co.uk>
2018-05-13 16:33:22 +0100

    basic layout and styling

--- src/App.js
+++ src/App.js
@@ -2,11 +2,27 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import Slider from './Slider';
+
 class App extends Component {
+  state = {
+    correct: false,
+    question: 'What is the answer?'
+  };
+
   render() {
+    const { correct, question } = this.state;
     return (
-      <div className="App">
-        <h1>Toggle Component</h1>
+      <div id="App">
+        <div id="Wrapper">
+          <h1 id="Question">{question}</h1>
+          <div id="Sliders">
+            <Slider />
+            <Slider />
+            <Slider />
+          </div>
+          <h2 id="Answer">The answer is {correct ? 'correct!' : 'incorrect.'}</h2>
+        </div>
       </div>
     );
   }
---
+++ src/Slider.js
@@ -0,0 +1,14 @@
+// @flow
+import React from 'react';
+
+import './Slider.css';
+
+const Slider = () => (
+  <div className="slider-outer">
+    <div className="slider-inner" />
+    <h3 className="text-left">Text left</h3>
+    <h3 className="text-right">Text right</h3>
+  </div>
+);
+
+export default Slider;
